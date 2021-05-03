c9a050a7ff7b66efcf95ea325255ea7589b86317
MrAlien3399 <zigz5638@gmail.com>
2021-05-03 19:57:06 +0630

    Update

--- send_mail.py
+++ send_mail.py
@@ -6,6 +6,6 @@ attachment_file = "git.png"
 
 to = 'zigz3883@gmail.com,zz5525492@gmail.com'
 
-userdata = {"zigz5638@gmail.com":""} #enter password here
+userdata = {"zigz5638@gmail.com":""} #enter password here. Gmail > Manage Accounts > App Password > Generate
 zigz = SendMail('smtp.gmail.com',465,to_send)
 zigz.send_email('zigz5638@gmail.com',to,userdata)
