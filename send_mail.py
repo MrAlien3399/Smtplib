bfd039e059266f8f9a715ed0fb6ffb6752c796b7
ZiGZ <zigz@zigz.ants>
2021-05-06 16:24:20 +0630

    Update SMTP

--- send_mail.py
+++ send_mail.py
@@ -6,6 +6,6 @@ attachment_file = "git.png,superman.jpg"
 
 to = 'zigz3883@gmail.com,zz5525492@gmail.com'
 
-userdata = {"zigz5638@gmail.com":""} #enter password here. Gmail > Manage Accounts > App Password > Generate > Mail = Winows App
+userdata = {"zigz5638@gmail.com":""} #enter password here. Gmail > Manage Accounts > App Password > Generate > Mail = Winows Computer
 zigz = SendMail('smtp.gmail.com',465,text_message,attachment_file)
 zigz.send_email('zigz5638@gmail.com',to,userdata)
